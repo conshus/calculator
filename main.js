e44987a43740fc854aac1e1ed5597861e89b01fe
Dwane <Dwane@hiphopandcode.com>
2017-03-24 15:40:10 -0400

    explorer mode accomplished

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 function calculate() {
+  total = 0;
+  console.log("calculationArray right after calculate fn", calculationArray);
   for (i=0; i < calculationArray.length; i++) {
     if (calculationArray[i] == "+"){
-      total = total + calculationArray[i+1];
+      total = total + parseFloat(calculationArray[i+1]);
       i++;
     } else if (calculationArray[i] == "-"){
-      total = total - calculationArray[i+1];
+      total = total - parseFloat(calculationArray[i+1]);
       i++;
     } else if (calculationArray[i] == "x"){
-      total = total * calculationArray[i+1];
+      total = total * parseFloat(calculationArray[i+1]);
       i++;
     } else if (calculationArray[i] == "÷"){
-      total = total / calculationArray[i+1];
+      total = total / parseFloat(calculationArray[i+1]);
       i++;
     }
     else {
-      total = total + calculationArray[i];
+      total = total + parseFloat(calculationArray[i]);
     }
   }
   //let final = eval(total);
@@ -22,12 +24,29 @@ function calculate() {
   //console.log("total: ", total);
   //console.log("final: ", final);
   display.textContent = total;
+  console.log("calculationArray before clearing after calculation", calculationArray);
+  calculationArray=[];
+  //calculationArray=[total];
+  entry = total.toString();
+  console.log("entry total.toString: ", entry);
+  console.log("after calculate calculationArray", calculationArray);
+  //entry = "";
+  //entry = total;
+  //console.log(entry);
 }
 function addToDisplay(event) {
   let buttonPressed = event.target.textContent;
   if ((buttonPressed == "÷") || (buttonPressed == "x") || (buttonPressed == "-") || (buttonPressed == "+")){
     if (display.textContent != ""){
-      calculationArray[calculationArray.length]=parseInt(entry);
+
+      if ((calculationArray[calculationArray.length-1] == "") && ((calculationArray[calculationArray.length-2] == "÷") || (calculationArray[calculationArray.length-2] == "x") || (calculationArray[calculationArray.length-2] == "-") || (calculationArray[calculationArray.length-2] == "+"))) {
+        //calculationArray.splice(calculationArray.length-1,2);
+        calculationArray.splice(calculationArray.length-2,2);
+        console.log("double operations ", calculationArray);
+      }
+
+
+      calculationArray[calculationArray.length]=entry;
       calculationArray[calculationArray.length]=buttonPressed;
       entry = "";
     }
@@ -37,31 +56,46 @@ function addToDisplay(event) {
     calculationArray=[];
     total = 0;
     //console.log("entry cleared");
-  }else if (buttonPressed == "."){
-    buttonPressed = "0"+buttonPressed+"0";
-    console.log(". pressed", buttonPressed);
   }else if (buttonPressed == "="){
-    //console.log(calculationArray[calculationArray.length-1]);
-    if ((calculationArray[calculationArray.length] == "÷") || (calculationArray[calculationArray.length] == "x") || (calculationArray[calculationArray.length] == "-") || (calculationArray[calculationArray.length] == "+")){
-      calculationArray.splice(calculationArray.length-1,2);
+    calculationArray[calculationArray.length]=entry;
+    if (display.textContent != ""){
+      //calculationArray[calculationArray.length]=parseFloat(entry);
+      console.log("calculationArray after = is pressed:", calculationArray);
+      console.log("the last value in calculationArray: ", calculationArray[calculationArray.length-1]);
+      if ((calculationArray[calculationArray.length-1] == "") && ((calculationArray[calculationArray.length-2] == "÷") || (calculationArray[calculationArray.length-2] == "x") || (calculationArray[calculationArray.length-2] == "-") || (calculationArray[calculationArray.length-2] == "+"))) {
+        //calculationArray.splice(calculationArray.length-1,2);
+        calculationArray.splice(calculationArray.length-2,2);
+        console.log("calculationArray after splice: ", calculationArray)
+        console.log("array length if last position is an operator: ", calculationArray.length);
+      }
+    }else if(display.textContent==""){
+      console.log("display is empty and = was pressed.");
+      total = 0;
+      calculationArray=[0];
     }else {
       console.log("entry: ",entry);
-      calculationArray[calculationArray.length]=parseInt(entry);
+      calculationArray[calculationArray.length]=entry;
+      console.log("array length if last position is not an operator: ", calculationArray.length);
+      console.log("calculationArray: ", calculationArray)
     }
-    calculate();
+    //calculationArray[calculationArray.length]=parseFloat(entry);
+    if (display.textContent != ""){
+      calculate();
+    }
   }else {
     entry = entry + buttonPressed;
     display.textContent = entry;
     //calculationArray[calculationArray.length]=parseInt(buttonPressed);
     //calculationArray[calculationArray.length]=parseInt(entry);
-    console.log(entry);
+    console.log("entry with no operators",entry);
   }
   //calculationArray[calculationArray.length] = buttonPressed;
   //calculationArray[calculationArray.length] = entry;
   //console.log(entry);
   //console.log(buttonPressed);
-  console.log(calculationArray);
+  //console.log(calculationArray);
 }
+let operators = ""
 let calculationArray = [];
 let display = document.querySelector("#answer");
 let buttons = document.querySelectorAll("button");
